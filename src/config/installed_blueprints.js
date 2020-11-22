c667e714eb4ae23f8ac6095ef439bb2ead659853
Crowdbotics <team@crowdbotics.com>
2020-11-22 00:21:21 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'UserProfile177281', human_name: 'User Profile', access_route: 'UserProfile177281'},
 { name: 'Tutorial177280', human_name: 'Tutorial', access_route: 'Tutorial177280', icon: 'question-circle'},
 { name: 'NotificationList177252', human_name: 'Notification List', access_route: 'NotificationList177252'},
-{ name: 'Settings177251', human_name: 'Settings', access_route: 'Settings177251'},
 { name: 'Settings177243', human_name: 'Settings', access_route: 'Settings177243'},
 { name: 'UserProfile177241', human_name: 'User Profile', access_route: 'UserProfile177241'},
 
