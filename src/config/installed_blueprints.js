853f8484894f7dbdfa979e54a1f62ed7fbe1a4fd
Crowdbotics <team@crowdbotics.com>
2020-11-22 00:44:59 +0000

    remove package blueprint-react-native-notifications-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -5,7 +5,6 @@ export const installed_blueprints = [
 { name: 'CalendarView70177313', human_name: 'Calendar View 70', access_route: 'CalendarView70177313', icon: 'calendar'},
 { name: 'UserProfile177281', human_name: 'User Profile', access_route: 'UserProfile177281'},
 { name: 'Tutorial177280', human_name: 'Tutorial', access_route: 'Tutorial177280', icon: 'question-circle'},
-{ name: 'NotificationList177252', human_name: 'Notification List', access_route: 'NotificationList177252'},
 { name: 'Settings177243', human_name: 'Settings', access_route: 'Settings177243'},
 
   // you can add more installed blueprints here
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -9,7 +9,6 @@ import SignUp14177319Navigator from '../features/SignUp14177319/navigator';
 import CopyOfSettings177314Navigator from '../features/CopyOfSettings177314/navigator';
 import CalendarView70177313Navigator from '../features/CalendarView70177313/navigator';
 import UserProfile177281Navigator from '../features/UserProfile177281/navigator';
-import NotificationList177252Navigator from '../features/NotificationList177252/navigator';
 import Settings177243Navigator from '../features/Settings177243/navigator';
 
 /**
@@ -23,7 +22,6 @@ SignUp14177319: { screen: SignUp14177319Navigator },
 CopyOfSettings177314: { screen: CopyOfSettings177314Navigator },
 CalendarView70177313: { screen: CalendarView70177313Navigator },
 UserProfile177281: { screen: UserProfile177281Navigator },
-NotificationList177252: { screen: NotificationList177252Navigator },
 Settings177243: { screen: Settings177243Navigator },
 
     /** new navigators can be added here */
