fcf1738854871db7e76e52e1f2d20b332e9c3152
Crowdbotics <team@crowdbotics.com>
2020-11-22 00:21:10 +0000

    add package blueprint-react-native-calendar-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import CalendarView70177313Navigator from '../features/CalendarView70177313/navigator';
 import UserProfile177281Navigator from '../features/UserProfile177281/navigator';
 import Tutorial177280Navigator from '../features/Tutorial177280/navigator';
 import NotificationList177252Navigator from '../features/NotificationList177252/navigator';
@@ -19,6 +20,7 @@ import UserProfile177241Navigator from '../features/UserProfile177241/navigator'
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+CalendarView70177313: { screen: CalendarView70177313Navigator },
 UserProfile177281: { screen: UserProfile177281Navigator },
 Tutorial177280: { screen: Tutorial177280Navigator },
 NotificationList177252: { screen: NotificationList177252Navigator },
--- src/redux/mainSaga.js
+++ src/redux/mainSaga.js
@@ -2,6 +2,7 @@ import { all, takeEvery, take } from "redux-saga/effects";
 
 
 //@BlueprintReduxSagaImportInsertion
+import CalendarView70177313Saga from '../features/CalendarView70177313/redux/sagas';
 
 function* helloSaga() {
   console.log("Hello from saga!");
@@ -14,6 +15,7 @@ export function* mainSaga() {
 
 
     //@BlueprintReduxSagaMainInsertion
+CalendarView70177313Saga,
     
   ]);
 }
